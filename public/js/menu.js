1f1e595c9dcada54587c1398518657dca6a19cc7
Alex <asergy34@gmail.com>
2018-11-14 19:38:23 +0300

    Edit menu js

--- public/js/menu.js
+++ public/js/menu.js
@@ -180,7 +180,7 @@ $(document).ready(function () {
 });
 
 /* Убрать прелоадер после полной загрузки страницы (включая загрузку картинок) */
-$(window).load(function () {
+$(window).ready(function () {
     $('.preloader').addClass('done');
 });
 
