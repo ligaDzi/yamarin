b3c7325d70d6e7a693b985d48e7ab343f988aab0
Alex <asergy34@gmail.com>
2018-12-06 15:22:50 +0300

    min css and js

--- public/js/myModernaizer.js
+++ public/js/myModernaizer.js
@@ -1,16 +1,1 @@
-// Если браузер не поддерживает атрибут placeholder
-if (!Modernizr.input.placeholder) {
-
-    $("input[placeholder]").focus(function () {
-        if ($(this).val() == $(this).attr('placeholder')) {
-            $(this).val("");
-        }
-    });
-
-    $("input[placeholder]").blur(function () {
-        if ($(this).val() == "") {
-            $(this).val($(this).attr('placeholder'));
-        }
-    });
-
-}
+Modernizr.input.placeholder||($("input[placeholder]").focus(function(){$(this).val()==$(this).attr("placeholder")&&$(this).val("")}),$("input[placeholder]").blur(function(){""==$(this).val()&&$(this).val($(this).attr("placeholder"))}));
